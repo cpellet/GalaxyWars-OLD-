d23169cfa57397a9721b4a420eed432b88525db2
Lumina|Erik <erik.nivala@yahoo.de>
2018-06-15 16:00:43 +0200

    Initial Commit

---
+++ Assets/WormsGame/Resources/Experimental Networking/Assets/Prefabs/Player/Scripts/uPlayer.cs
@@ -0,0 +1,58 @@
+using uDoil;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class uPlayer : NetworkBehaviour
+{
+    [SerializeField] private float speed;
+    private int owner => GetComponent<NetworkIdentity>().playerControllerId;
+    private CharacterController controller => GetComponent<CharacterController>();
+    private Vector3 direction;
+    private float gravity = -0.1f;
+    [SerializeField] private float gravityStrength = 2;
+
+    public void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        direction = new Vector2(owner.GetAxis("L_VERTICAL"), owner.GetAxis("L_HORIZONTAL")).normalized;
+
+        if (controller.isGrounded)
+        {
+            gravity = -0.1f;
+        }
+
+        gravity -= gravityStrength * Time.deltaTime;
+        controller.Move(new Vector3(direction.x, gravity, direction.y) * speed * Time.deltaTime);
+    }
+
+    private void OnGUI()
+    {
+        if (isServer)
+        {
+            GUI.Label(new Rect(10, 10, 500, 50), $"Connections: {playerCount()} \nSlots used: {slotsInUse()}");
+        }
+    }
+
+    public int slotsInUse()
+    {
+        int counter = 0;
+
+        foreach (NetworkConnection ply in NetworkServer.connections)
+        {
+            if (ply != null)
+                if (ply.clientOwnedObjects != null)
+                    counter += Mathf.Clamp(ply.clientOwnedObjects.Count, 1, 4);
+                else
+                    counter += 1;
+        }
+        return counter;
+    }
+
+    public int playerCount()
+    {
+        return NetworkServer.connections.Count;
+    }
+}
---
+++ Assets/WormsGame/Resources/Scripts/Player/LocomotionSMB.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using uDoil;
+
+public class LocomotionSMB : StateMachineBehaviour
+{
+
+    public float m_Damping = 0.15f;
+
+    // private readonly int m_HashHorizontalPara = Animator.StringToHash ("Horizontal");
+    // private readonly int m_HashVerticalPara = Animator.StringToHash ("Vertical");
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float horizontal = 0.GetAxis("L_HORIZONTAL");
+        float vertical = 0.GetAxis("L_VERTICAL");       
+
+        //   Vector2 input = new Vector2 (horizontal, vertical).normalized;
+
+
+        //  animator.SetFloat ("Horizontal", input.x, m_Damping, Time.deltaTime);
+        //   animator.SetFloat ("Vertical", input.y, m_Damping, Time.deltaTime);
+        animator.SetFloat("Horizontal", horizontal, m_Damping, Time.deltaTime);
+        animator.SetFloat("Vertical", vertical, m_Damping, Time.deltaTime);
+
+    }
+}
