34141ac73b25837c964011122e72d0e7e1be14c7
cpellet <cyrus135@hotmail.fr>
2018-06-26 07:37:41 +0200

    Added weapon switching mechanics
    demo can be found in the alpa-mechanics folder

---
+++ Assets/WeaponSwitching.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour {
+
+	public int activeSlot = 0;
+
+	// Use this for initialization
+	void Start () {
+		SelectSlot();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int previousSelectedSlot = activeSlot;
+
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f){
+			if (activeSlot >= transform.childCount -1){
+				activeSlot = 0;
+			}else{
+				activeSlot++;
+			}
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") < 0f){
+			if (activeSlot <= 0){
+				activeSlot = transform.childCount -1;
+			}else{
+				activeSlot--;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha1)){
+			activeSlot = 0;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2){
+			activeSlot = 1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3){
+			activeSlot = 2;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4){
+			activeSlot = 3;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha5) && transform.childCount >= 5){
+			activeSlot = 4;
+		}
+
+		if (previousSelectedSlot != activeSlot){
+			SelectSlot();
+		}
+	}
+
+	void SelectSlot(){
+		int i = 0;
+		foreach (Transform slot in transform){
+			if (i == activeSlot){
+				slot.gameObject.SetActive(true);
+			}else{
+				slot.gameObject.SetActive(false);
+			}
+			i++;
+		}
+	}
+}
