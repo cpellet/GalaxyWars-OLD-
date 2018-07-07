5a05ac60e085885eb9b0a094cabd3d44b5c31067
cpellet <cyrus135@hotmail.fr>
2018-07-07 12:05:13 +0200

    Added Main menu and UI elements

---
+++ Assets/Alpha-Mechanics/MainMenu/LoadingManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingManager : MonoBehaviour {
+
+	public Slider slider;
+	public Text percentageText;
+	public Canvas mainMenu;
+	public Canvas loadingCanvas;
+
+	public void StartLoading(int sceneIndex)
+	{
+		mainMenu.gameObject.SetActive(false);
+		loadingCanvas.gameObject.SetActive(true);
+		LoadLevel(sceneIndex);
+	}
+
+	public void LoadLevel(int sceneIndex)
+	{
+		StartCoroutine(LoadAsynchronously(sceneIndex));
+	}
+
+	IEnumerator LoadAsynchronously(int sceneIndex)
+	{
+		// Store loading async operation into object we can access
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+		//Update UI with progress while operation is running
+		while(!operation.isDone)
+		{
+			//Ignore Unity's activation process
+			float progress = Mathf.Clamp01(operation.progress / .9f);
+			slider.value = progress;
+			if (progress >= .2f){
+				percentageText.text = progress * 100f + "%";
+			}
+			yield return null;
+		}
+	}
+}
